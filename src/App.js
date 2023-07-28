bb9e35f61f91a970b6b32e656c9a6e9d5098f1a8
Rutuja Hire <119806384+rutujahire65@users.noreply.github.com>
2023-07-28 12:03:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// import Resturant from './component/Basics/Resturant'
+// import Menu from './component/Basics/menuApi'
+// import UseState from './component/Hooks/useState'
+// import  UseEffect from "./component/Hooks/useEffect"
+// import UseReducer from './component/Hooks/useReducer'
+import Todo from './component/todoReact/todo'
+// import Temp from './component/weather/temp'
+const App = () => {
+  return (
+   <div>
+             
+      {/* <UseEffect/> */}
+      {/* <UseReducer/> */}
+      {/* <Resturant/> */}
+      <Todo/>
+      {/* <Temp/>  */}
+   </div>     
+  )
+}
+                                                             
+export default App
---
+++ src/component/weather/weathercard.js
@@ -0,0 +1,96 @@
+import React, { useEffect } from 'react'
+
+const Weathercard = ({tempInfo}) => {
+    const [weathearState,setWeatheState] =React.useState("");
+
+    const {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+         sunset,
+    } =tempInfo;
+
+     useEffect(()=>{
+        if(weathermood){
+            switch(weathermood){
+                case 'Clouds':setWeatheState("wi-day-cloudy");
+                    break;
+                    case 'Haze':setWeatheState("wi-fog");
+                    break;
+                    case 'Clear':setWeatheState("wi-day-sunny");
+                    break;
+                    case 'Mist':setWeatheState("wi-dust");
+                    break;
+                    default:
+                        setWeatheState("wi-day-sunny");
+                        break;
+            }
+        }
+     },[weathermood]);
+
+    // converting seconds into time
+
+    let sec = sunset;
+    let date = new Date(sec *1000);
+    let timeStr = `${date.getHours()}:${date.getMinutes()}`
+  return (
+    <>
+        <article className='widget'>
+                <div className='weatherIcon'>
+                    <i className={`wi ${weathearState}` }></i>
+                </div>
+                <div className='weatherInfo'>
+                    <div className='temperature'>
+                        <span>{temp}&deg;</span>
+                        <div className='description'>
+                            <div className='weatherCondition'>{weathermood}</div>
+                            <div className='place'> {name}, {country}</div>
+                        </div>
+                    </div>
+                </div>
+                <div className='date'>{new Date().toLocaleString()}</div>
+                {/* our 4colomn section */}
+                <div className='extra-temp'>
+                    <div className='temp-info-minmax'>
+                        <div className='two-sided-section'>
+                            <p><i className={'wi wi-sunset'}></i></p>
+                            <p className='extra-info-leftside'>
+                            {timeStr}  pm <br />
+                                sunset
+                            </p>
+                        </div>
+                        <div className='two-sided-section'>
+                            <p><i className={'wi wi-humidity'}></i></p>
+                            <p className='extra-info-leftside'>
+                            {humidity}  <br />
+                                Humidity
+                            </p>
+                        </div>
+                    </div>
+                    <div className='weather-extra-info'>
+                        <div className='two-sided-section'>
+                            <p><i className={'wi wi-rain'}></i></p>
+                            <p className='extra-info-leftside'>
+                            {pressure}  <br />
+                                Pressure
+                            </p>
+                        </div>
+                        <div className='two-sided-section'>
+                            <p><i className={'wi wi-strong-wind'}></i></p>
+                            <p className='extra-info-leftside'>
+                               {speed} <br />
+                                Speed
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+    </>
+  )
+}
+
+export default Weathercard
